9ae6725edcb33dda7e26e825f6995bd52641bcde
jhossepmartinez <jhossepmv@gmail.com>
2024-05-06 00:04:05 -0400

    Added new health route and fixed indent

--- backend/src/controllers/routes.js
+++ backend/src/controllers/routes.js
@@ -1,18 +1,20 @@
-import UserController from './UserController.js';
-import SolicitudesController from './SolicitudesController.js'
+import UserController from "./UserController.js";
+import SolicitudesController from "./SolicitudesController.js";
 
 export default (app) => {
-	const userController = new UserController();
-    const solicitudesController = new SolicitudesController();
+  const userController = new UserController();
+  const solicitudesController = new SolicitudesController();
 
-	app.get('/users', userController.getAll);
-	app.post('/users', userController.create);
-	app.get('/users/:userId', userController.get);
-	app.put('/users/:userId', userController.update);
-	app.delete('/users/:userId', userController.delete);
-    app.get('/solicitudes', solicitudesController.getAll);
-    app.post('/solicitudes', solicitudesController.create);
-    app.get('/solicitudes/:solicitudesId', solicitudesController.get);
-    app.put('/solicitudes/:solicitudesId', solicitudesController.update)
-
+  app.get("/users", userController.getAll);
+  app.post("/users", userController.create);
+  app.get("/users/:userId", userController.get);
+  app.put("/users/:userId", userController.update);
+  app.delete("/users/:userId", userController.delete);
+  app.get("/solicitudes", solicitudesController.getAll);
+  app.post("/solicitudes", solicitudesController.create);
+  app.get("/solicitudes/:solicitudesId", solicitudesController.get);
+  app.put("/solicitudes/:solicitudesId", solicitudesController.update);
+  app.get("/test", (req, res) => {
+    res.send("Backend is working");
+  });
 };
