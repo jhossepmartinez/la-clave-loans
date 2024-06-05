3f2548e127f1a91097d724219d7d149e425e3f36
jhossepmartinez <jhossepmv@gmail.com>
2024-06-05 00:22:25 -0400

    Now keeps the alert message on solicitud submit

--- frontend/src/views/calculator/calculator.jsx
+++ frontend/src/views/calculator/calculator.jsx
@@ -75,6 +75,7 @@ export default function calculator() {
 	const [show_alert, set_show_alert] = useState(false);
 	const [display_download_button, set_download_button] = useState(false);
 	const [submitedSolicitudId, setSubmitedSolicitudId] = useState(null);
+	const [show_alert_error, set_show_alert_error] = useState(false);
 
 	const submitForm = async (e) => {
 		e.preventDefault();
@@ -83,12 +84,15 @@ export default function calculator() {
 			const response = await createSolicitud(state);
 			setSubmitedSolicitudId(response.data.solicitud.id);
 			// history.push(`/solicitudes/${response.data.id}`);
+			set_show_alert_error(false);
 			set_show_alert(true);
 			set_download_button(true);
-			setTimeout(() => {
-				set_show_alert(false);
-			}, 3000);
+			// setTimeout(() => {
+			// 	set_show_alert(false);
+			// }, 3000);
 		} catch (error) {
+			set_show_alert(false);
+			set_show_alert_error(true);
 			console.log(error);
 			alert("A ocurrido un error al actualizar");
 		}
@@ -434,6 +438,11 @@ export default function calculator() {
 									<div className="alert alert-success" role="alert">
 										Solicitud enviada correctamente! codigo solicitud{" "}
 										{submitedSolicitudId}
+									</div>
+								)}
+								{show_alert_error && (
+									<div className="alert alert-error" role="alert">
+										Solicitud no se pudo enviar correctamente!
 									</div>
 								)}
 							</div>
